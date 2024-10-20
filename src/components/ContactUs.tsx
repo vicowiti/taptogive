1e63ab289b020b24927f64f0908b76bb7495d293
Victor <victor.owiti@zeraki.app>
2024-10-20 22:15:16 +0300

    footer and contact page ui

---
+++ src/components/ContactUs.tsx
@@ -0,0 +1,119 @@
+
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/outline";
+
+export default function ContactUs() {
+  return (
+    <div className="relative bg-[#e6f9f0]">
+      <div className="absolute inset-0">
+        <div className="absolute inset-y-0 left-0 w-1/2 bg-[#e6f9f0]" />
+      </div>
+      <div className="relative mx-auto max-w-7xl lg:grid lg:grid-cols-5">
+        <div className="bg-[#e6f9f0] py-16 px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
+          <div className="mx-auto max-w-lg">
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+              Get in touch
+            </h2>
+            <p className="mt-3 text-lg leading-6 text-gray-500">
+              Reach out and lets make giving effortless.
+            </p>
+            <dl className="mt-8 text-base text-gray-500">
+              <div>
+                <dt className="sr-only">Postal address</dt>
+                <dd>
+                  <p>Regent Court Apartments</p>
+                  <p>Argwings Kodhek</p>
+                </dd>
+              </div>
+              <div className="mt-6">
+                <dt className="sr-only">Phone number</dt>
+                <dd className="flex">
+                  <PhoneIcon
+                    className="h-6 w-6 flex-shrink-0 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  <span className="ml-3">+254 777 104 733</span>
+                </dd>
+              </div>
+              <div className="mt-3">
+                <dt className="sr-only">Email</dt>
+                <dd className="flex">
+                  <EnvelopeIcon
+                    className="h-6 w-6 flex-shrink-0 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  <span className="ml-3">info@tap2give.co.ke</span>
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+        <div className="bg-[#e6f9f0] py-16 px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
+          <div className="mx-auto max-w-lg lg:max-w-none">
+            <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
+              <div>
+                <label htmlFor="full-name" className="sr-only">
+                  Full name
+                </label>
+                <input
+                  type="text"
+                  name="full-name"
+                  id="full-name"
+                  autoComplete="name"
+                  className="block w-full outline-none rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                  placeholder="Full name"
+                />
+              </div>
+              <div>
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full outline-none rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                  placeholder="Email"
+                />
+              </div>
+              <div>
+                <label htmlFor="phone" className="sr-only">
+                  Phone
+                </label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  autoComplete="tel"
+                  className="block w-full outline-none rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                  placeholder="Phone"
+                />
+              </div>
+              <div>
+                <label htmlFor="message" className="sr-only">
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  className="block w-full outline-none  rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                  placeholder="Message"
+                  defaultValue={""}
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="inline-flex justify-center rounded-md border border-transparent bg-green-600 py-2 px-6 text-base font-medium text-white shadow-sm "
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer className="py-5 bg-white px-20 text-lg font-semibold flex flex-col lg:flex-row justify-between w-full">
+      <div className="flex justify-center items-center">
+        <Image src="/logo/logo.png" width={100} height={50} alt="logo" />
+      </div>
+      <section className="grid grid-cols-1 lg:grid-cols-3 gap-3">
+        <div className="flex justify-center items-center">
+          <p className="duration-300 hover:text-green-600">© 2023 Tap2Give</p>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <p className="duration-300 hover:text-green-600">
+            Phone: +254 777 104 733
+          </p>
+        </div>
+        <div className="flex justify-center items-center">
+          <p className="duration-300 hover:text-green-600">
+            Email: info@tap2give.co.ke
+          </p>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
